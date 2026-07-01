06fabf6d390a454dc635d5b2898b0dc2e549cb33
yeang976-art <yeang976@gmail.com>
2026-07-01 13:19:01 +0900

    v.1.3.2
    import 2차 정리

--- src/main/java/com/example/schedule_project/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/schedule_project/exception/GlobalExceptionHandler.java
@@ -1,9 +1,7 @@
 package com.example.schedule_project.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
