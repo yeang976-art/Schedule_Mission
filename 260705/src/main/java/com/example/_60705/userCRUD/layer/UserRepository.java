4c3939e273b263f4ef150414f8c2f0acb435428b
yeang976-art <yeang976@gmail.com>
2026-07-09 11:21:10 +0900

    v.1.5.1 버그 수정

--- 260705/src/main/java/com/example/_60705/userCRUD/layer/UserRepository.java
+++ 260705/src/main/java/com/example/_60705/userCRUD/layer/UserRepository.java
@@ -1,7 +1,6 @@
 package com.example._60705.userCRUD.layer;
 
 import com.example._60705.userCRUD.entity.User;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
